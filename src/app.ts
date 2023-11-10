e0d5c13533640d0de2dcd62cc72056ab05a58adf
lagairogo <70363482+lagairogo@users.noreply.github.com>
2023-11-11 00:58:50 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
+    // After successful login, please redirect back to the intended page
     if (!req.user &&
     req.path !== "/login" &&
     req.path !== "/signup" &&
