74b7dc312b9cbb36f83583587c6d1ba93bc65fb3
lagairogo <70363482+lagairogo@users.noreply.github.com>
2023-11-12 16:59:54 +0530

    Delete src/util/flagsmith.ts

--- src/util/flagsmith.ts
+++
@@ -1,8 +0,0 @@
-import Flagsmith from "flagsmith-nodejs";
-
-const flagsmith = new Flagsmith({
-  environmentKey: process.env.FLAGSMITH_KEY as string,
-  requestTimeoutSeconds: 60,
-});
-
-export default flagsmith;
