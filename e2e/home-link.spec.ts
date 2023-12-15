d427732e22c1f19ec1221ac4d5c041fa3fa200c2
lagairogo <70363482+lagairogo@users.noreply.github.com>
2023-12-15 07:15:13 +0000

    Update home-link.spec.ts

--- e2e/home-link.spec.ts
+++ e2e/home-link.spec.ts
@@ -4,7 +4,7 @@ test("should navigate to the about page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("http://localhost:3000/");
   // Find an element with the text 'Integrations' and click on it
-  await page.click("text=Integrations");
+  await page.click("text=Why Lagairogo");
   // The new URL should be "/about" (baseURL is used there)
   await expect(page).toHaveURL("http://localhost:3000/api");
   // The new page should contain an h1 with "About Page"
